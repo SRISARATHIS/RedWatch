0db55c9ed91b46e6b45e88aedbbc97fc2c592594
Kamaal Ahmed <kamaalahmed@Mac.fritz.box>
2026-02-02 17:01:26 +0100

    final

--- dbt/target/run/redwatch/models/kpis/kpi_leaderboard_15m.sql
+++ dbt/target/run/redwatch/models/kpis/kpi_leaderboard_15m.sql
@@ -5,7 +5,7 @@
             where (
                 lb_key) in (
                 select (lb_key)
-                from "kpi_leaderboard_15m__dbt_tmp161122957719"
+                from "kpi_leaderboard_15m__dbt_tmp232527993368"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "redset_db"."analytics"."kpi_leaderboard_15m" ("lb_key", "window_start", "window_end", "dimension_type", "dimension_value", "rank_position", "queries_count", "heavy_units_sum")
     (
         select "lb_key", "window_start", "window_end", "dimension_type", "dimension_value", "rank_position", "queries_count", "heavy_units_sum"
-        from "kpi_leaderboard_15m__dbt_tmp161122957719"
+        from "kpi_leaderboard_15m__dbt_tmp232527993368"
     )
   
--- dbt/target/run/redwatch/models/kpis/kpi_minute_shadow_cost.sql
+++ dbt/target/run/redwatch/models/kpis/kpi_minute_shadow_cost.sql
@@ -5,7 +5,7 @@
             where (
                 kpi_key) in (
                 select (kpi_key)
-                from "kpi_minute_shadow_cost__dbt_tmp161123229844"
+                from "kpi_minute_shadow_cost__dbt_tmp232528558191"
             );
 
         
@@ -14,6 +14,6 @@
     insert into "redset_db"."analytics"."kpi_minute_shadow_cost" ("kpi_key", "minute_ts", "instance_id", "queries_count", "heavy_units_sum", "cluster_size_clean_max", "cost_units_per_min", "cost_units_per_min_per_node")
     (
         select "kpi_key", "minute_ts", "instance_id", "queries_count", "heavy_units_sum", "cluster_size_clean_max", "cost_units_per_min", "cost_units_per_min_per_node"
-        from "kpi_minute_shadow_cost__dbt_tmp161123229844"
+        from "kpi_minute_shadow_cost__dbt_tmp232528558191"
     )
   
